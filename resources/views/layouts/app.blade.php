af3c41e8e9374a8b14aef4003bb51ac071ebe871
nikamodis <nikamodis@gmail.com>
2021-02-13 16:57:40 +0200

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Home</a>
+                            <a class="nav-link" href="/">Home</a>
                         </li>
                         <li class="nav-item active">
                             <a class="nav-link" href="#">Shop</a>
